2562340cb0cf658cf06f63069278e1f380cab4bc
sinxkyuna <143151109+shxzu@users.noreply.github.com>
2025-07-12 21:12:42 -0500

    do a thing

--- src/main/java/Start.java
+++ src/main/java/Start.java
@@ -7,8 +7,6 @@ public class Start {
 
     public static void main(String[] args)
     {
-        System.setProperty("org.lwjgl.librarypath", new File("../natives/" + (System.getProperty("os.name").startsWith("Windows") ? "windows" : "linux")).getAbsolutePath());
-
         Main.main(concat(new String[]{"--version", "GradleMCP", "--accessToken", "0", "--assetsDir", "assets", "--assetIndex", "1.8", "--userProperties", "{}"}, args));
     }
 
--- src/main/java/cc/simp/Simp.java
+++ src/main/java/cc/simp/Simp.java
@@ -27,7 +27,7 @@ public class Simp {
     //Simp Client Info
     public static final Simp INSTANCE = new Simp();
     public static final String NAME = "Simp";
-    public static final String BUILD = "070925";
+    public static final String BUILD = "071225";
 
     //Client Background Stuff
     private ModuleManager moduleManager;
