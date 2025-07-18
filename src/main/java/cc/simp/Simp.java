a6e2430a28f121ea5f638ec3cbf8efd39fe1e2e5
sinxkyuna <143151109+shxzu@users.noreply.github.com>
2025-07-17 21:13:14 -0500

    update ver

--- src/main/java/cc/simp/Simp.java
+++ src/main/java/cc/simp/Simp.java
@@ -27,7 +27,7 @@ public class Simp {
     //Simp Client Info
     public static final Simp INSTANCE = new Simp();
     public static final String NAME = "Simp";
-    public static final String BUILD = "071325";
+    public static final String BUILD = "071725";
 
     //Client Background Stuff
     private ModuleManager moduleManager;
