5b239dfb0e26969bbb3b7148255ed91ca2302ea5
xlunari <lukeyr09@gmail.com>
2025-10-20 11:13:53 -0500

    asd

--- src/main/java/cc/simp/utils/render/ESPUtils.java
+++ src/main/java/cc/simp/utils/render/ESPUtils.java
@@ -8,6 +8,7 @@ import net.minecraft.client.renderer.vertex.DefaultVertexFormats;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.util.AxisAlignedBB;
 import net.minecraft.util.MathHelper;
 import org.lwjgl.opengl.GL11;
 
