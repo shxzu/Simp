dbe92ca457661ea72ff4edd125048a696c8c2aa2
shxzu <143151109+shxzu@users.noreply.github.com>
2026-01-14 21:07:03 -0600

    1.1

--- src/main/java/cc/simp/Simp.java
+++ src/main/java/cc/simp/Simp.java
@@ -28,7 +28,7 @@ public class Simp {
     public static final Simp INSTANCE = new Simp();
     public static final String NAME = "Simp";
     public static final String BUILD = BuildType.RELEASE.getName();
-    public static final String VERSION = "1.0 " + BUILD;
+    public static final String VERSION = "1.1 " + BUILD;
     public static final String FULL = NAME + " " + VERSION;
 
     private EventBus<Event> eventBus;
