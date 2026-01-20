62cc61edf9155a2a50aa771ebb89d85f23565979
shxzu <143151109+shxzu@users.noreply.github.com>
2026-01-19 21:54:03 -0600

    build status: dev to release

--- src/main/java/cc/simp/Simp.java
+++ src/main/java/cc/simp/Simp.java
@@ -27,7 +27,7 @@ import java.util.Arrays;
 public class Simp {
     public static final Simp INSTANCE = new Simp();
     public static final String NAME = "Simp";
-    public static final String BUILD = BuildType.DEV.getName();
+    public static final String BUILD = BuildType.RELEASE.getName();
     public static final String VERSION = "1.2 " + BUILD;
     public static final String FULL = NAME + " " + VERSION;
 
